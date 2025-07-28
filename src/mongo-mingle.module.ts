f0afed40063b16ed126b31ee46759cfc20c5278f
hhfrancois <francois.achache@gmail.com>
2025-07-28 21:53:11 +0200

    Ajout de la possibilité de passer des options à MongoClient dans la méthode forRoot de MongoMingleModule.

--- src/mongo-mingle.module.ts
+++ src/mongo-mingle.module.ts
@@ -1,14 +1,14 @@
 import { DynamicModule, Module } from "@nestjs/common";
-import { MongoClient } from "mongodb";
+import { MongoClient, MongoClientOptions } from "mongodb";
 import { MongoMingle } from "./mongo-mingle";
 
 @Module({})
 export class MongoMingleModule {
-  static forRoot({uri}: {uri: string}): DynamicModule {
+  static forRoot(uri: string, options?: MongoClientOptions): DynamicModule {
     return {
       module: MongoMingleModule,
       providers: [
-        { provide: MongoClient, useFactory: async () => new MongoClient(uri),},
+        { provide: MongoClient, useFactory: async () => new MongoClient(uri, options)},
         MongoMingle
       ],
       exports: [MongoMingle]
